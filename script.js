bf07cdd2940048aec062f23ab90bc154949e4c21
Garrett Anderson <keaton85@runbox.com>
2021-06-23 17:34:32 -0500

    grab elements

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ var endTime;
 var msLeft;
 var minutes;
 var seconds;
+
+const time_left = document.getElementById("time-left");
+const start_stop = document.querySelector("#start_stop");
+const break_increment = document.getElementById("break-increment");
+const break_decrement = document.getElementById("break-decrement");
+const session_increment = document.getElementById("session-increment");
+const session_decrement = document.getElementById("session-decrement");
+const reset = document.querySelector("#reset");
+const break_length = document.querySelector("#break-length");
+const session_length = document.querySelector("#session-length");
+const timer_label = document.querySelector("#timer-label");
+const alarm = document.getElementById("beep");
